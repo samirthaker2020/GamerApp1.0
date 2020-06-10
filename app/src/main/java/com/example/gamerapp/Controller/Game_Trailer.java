2db78993cbb7e36fffe7eb1fa5d4d2d40a70bb20
JOKERPC1 <samir2009thaker@gmail.com>
2020-06-10 12:51:10 -0400

    trailer video displayed sucessfully

--- app/src/main/java/com/example/gamerapp/Controller/Game_Trailer.java
+++ app/src/main/java/com/example/gamerapp/Controller/Game_Trailer.java
@@ -2,8 +2,12 @@ package com.example.gamerapp.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.MediaController;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.gamerapp.R;
 
@@ -20,6 +24,17 @@ String gametrailer;
             gametrailer = extras.getString("gametrailer");
             // and get whatever type user account id is
             Toast.makeText(getApplicationContext(),gametrailer, Toast.LENGTH_SHORT).show();
+            getSupportActionBar().hide();
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+            String vidAddress =  gametrailer;
+            Uri vidUri = Uri.parse(vidAddress);
+            vidView.setVideoURI(vidUri);
+            vidView.start();
+            MediaController vidControl = new MediaController(this);
+            vidControl.setAnchorView(vidView);
+            vidView.setMediaController(vidControl);
         }
     }
 }
