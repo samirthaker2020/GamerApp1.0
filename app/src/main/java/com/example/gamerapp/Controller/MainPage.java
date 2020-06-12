908c4ab68bf4a20c81a94a23b1bfa09fd3ed46f0
JOKERPC1 <samir2009thaker@gmail.com>
2020-06-11 23:45:17 -0400

    icon modification

--- app/src/main/java/com/example/gamerapp/Controller/MainPage.java
+++ app/src/main/java/com/example/gamerapp/Controller/MainPage.java
@@ -37,7 +37,7 @@ public class MainPage extends AppCompatActivity   implements NavigationView.OnNa
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_reviewhistory,R.id.nav_aboutus,R.id.nav_logout)
+                R.id.nav_home, R.id.nav_profile, R.id.nav_reviewhistory,R.id.nav_aboutus,R.id.nav_logout,R.id.nav_contactus,R.id.nav_needhelp)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
